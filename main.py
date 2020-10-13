c892f845f9a15b5de7e3505cc2d204e1f93b235a
Luk3-S <lukesp63@gmail.com>
2020-10-13 16:32:35 +0100

    Removing triggered reminders seems to work

--- main.py
+++ main.py
@@ -16,11 +16,7 @@ def build_parameters(path):
 
         params[line[0]] = line[1:]
         remind_intervals.append(line[1])
-    
-    # for k,v in params.items():
-    #     print (k,v)
-    # for i in remind_intervals:
-    #     print(i)
+   
     return params,remind_intervals
 
 
@@ -29,25 +25,10 @@ def is_time_between(begin_time, end_time, check_time=None):
     # If check time is not given, default to current UTC time
     check_time = check_time or datetime.utcnow().time()
     if begin_time < end_time:
-        #print("check | beg | end")
-        #print("{},{},{}".format(check_time,begin_time,end_time))
-        #print(check_time >= begin_time and check_time <= end_time)
         return check_time >= begin_time and check_time <= end_time
     else: # crosses midnight
         return check_time >= begin_time or check_time <= end_time
 
-
-#run code continually throughout the working day
-
-#params: 
-# parameter: remindPeriod, other parameter
-params,remind_intervals = build_parameters(os.getcwd()+"/config.csv")
-
-
-
-#while (is_time_between(time(8,30), time(17,00))):
-# start = time(8,30)
-# next_reminder = []
 
 
 
@@ -59,53 +40,43 @@ def generate_reminders(remind_intervals,params):
         endtime = datetime(100,1,1,17,30,00)
         curr = datetime(100,1,1,8,30,00)
         while (curr < endtime):
-            #print("k: {} ,v: {}".format(k,v[0]))
             curr = curr + timedelta(minutes=int(v[0]))
             reminder_info.append(curr.time())
         v.append(reminder_info)
-        #print(v)
-        #print("\n")
-    
-    
-generate_reminders(remind_intervals,params)
+   
 
 
-#while (1==1):
+if __name__ == "__main__":
+    params,remind_intervals = build_parameters(os.getcwd()+"/config.csv")    
+    generate_reminders(remind_intervals,params)
 
-to_call = {}
-while (1==1):
-    for k,v in params.items():
-
-        #to_remove =[]
-        #print ("\n")
-        #print(k,v)
-        
-        #print(v)
-        # print("arg:")
-        # print(v[len(v)])
-        # print("now:")
-        # print(datetime.now().time())
-        for i in range(len(v[len(v)-1])):
-            #print(i)
-            current = datetime.now()
-            leeway = current + timedelta(minutes=3)
-            current =current.time()
-            leeway=leeway.time()
-            #print(leeway)
-            #check if current time is within a certain leeway or equal to a reminder time 
-            if (is_time_between(current,leeway,v[len(v)-1][i]) or v[len(v)-1][i] == datetime.now().time()):
-                #to_call.append("{}: {}".format(k,v[len(v)-1][i]))
-                #to_call.append("{}, {}".format(k,v[:len(v)-1]))
-                to_call[k] = v[:len(v)-1]
-                
-
-
-
-        print("v after: {}".format(v))
-        print("to_call: {}".format(to_call))   
-    
-
-    if (len(to_call)>0):
-        generatePopup(to_call)
     to_call = {}
-    time.sleep(180)
+    to_rmv =[]
+    while (1==1):
+        for k,v in params.items():
+
+            for i in range(len(v[len(v)-1])):
+                #print(i)
+                current = datetime.now()
+                leeway = current + timedelta(minutes=30)
+                current =current.time()
+                leeway=leeway.time()
+
+                #check if current time is within a certain leeway or equal to a reminder time 
+                if (is_time_between(current,leeway,v[len(v)-1][i]) or v[len(v)-1][i] == datetime.now().time()):
+                    to_call[k] = v[:len(v)-1]
+                    to_rmv.append(i)
+
+            print("v after: {}".format(v))
+            print("to_call: {}".format(to_call)) 
+            print(to_rmv)  
+            for i in to_rmv:
+                print("i: ",i)
+                print("vlen ",v[len(v)-1][i])
+                del v[len(v)-1][i]
+            to_rmv=[]
+        if (len(to_call)>0):
+            generatePopup(to_call)
+        to_call = {}
+        #time.sleep(180)
+        time.sleep(60)
